199d90a9ace5fa1cec9e0ba59acb99a70e37eb42
lncitador <walaffonofre@icloud.com>
2020-11-02 13:40:28 -0300

    config eslint and server

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import express from 'express';
+
+const app = express();
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on http://localhost:3333');
+});
